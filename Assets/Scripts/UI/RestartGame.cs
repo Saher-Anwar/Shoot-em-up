f89d429f75c4a02957e3c85f56dd89701c5a7cc0
Hesham Elokdah <92018284+OkudaHeishi@users.noreply.github.com>
2022-11-28 06:40:57 -0400

    winning and losing

---
+++ Assets/Scripts/UI/RestartGame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //This should take the player to main menu, temporarily using desert
+    public void Retry()
+    {
+        SceneManager.LoadScene("Desert");
+    }
+}
